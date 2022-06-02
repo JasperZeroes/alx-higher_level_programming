8dca0553312e23e6c6ee98632368e535da412d87
JasperZeroes <jasperobed@gmail.com>
2022-06-02 15:19:39 +0000

    adds, subtracts, multiplies and divides a number

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     result2 = sub(a, b)
     result3 = mul(a, b)
     result4 = div(a, b)
-    
+
     print("{} + {} = {}".format(a, b, result))
     print("{} - {} = {}".format(a, b, result2))
     print("{} * {} = {}".format(a, b, result3))
