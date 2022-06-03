d4823c5e2d20b744fef663393467c796da5ee864
JasperZeroes <jasperobed@gmail.com>
2022-06-03 03:49:39 +0000

    added large numbers easily

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -11,29 +11,3 @@ if __name__ == "__main__":
     for i in range(1, count):
         result += int(sys.argv[i])
     print("{}".format(result))
-'''parser = argparse.ArgumentParser()
-    parser.add_argument("number1", help = "first number")
-    parser.add_argument("number2", help = "second number")
-    parser.add_argument("operation", help = "operation")
-
-    args = parser.parse_args()
-
-    print(args.number1)
-    print(args.number2)
-    print(args.operation)
-    print(len(args))
-
-    if args.operation == "add":
-        result = int(args.number1) + int(args.number2)
-        print("Result:", result)
-    elif args.operation == "subtract":
-        result = int(args.number1) - int(args.number2)
-        print("Result:", result)
-    elif args.operation == "multiply":
-        result = int(args.number1) * int(args.number2)
-        print("Result:", result)
-    elif args.operation == "division":
-        result = int(args.number1) / int(args.number2)
-        print("Result:", int(result))
-    else:
-        print("Unsupported Operation")'''
