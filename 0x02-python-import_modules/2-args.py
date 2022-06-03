845188e0687172a206948b79d0ae0caa91d6b2ad
JasperZeroes <jasperobed@gmail.com>
2022-06-03 03:20:31 +0000

    command line dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,15 @@
 if __name__ == "__main__":
     import sys
 
-    num = len(sys.argv)
-    if num == 1:
-        print("{} arguments.".format(num - 1))
-    elif num == 2:
-        print("{} argument:".format(num - 1))
-    else:
-        print("{} arguments:".format(num - 1))
+    count = len(sys.argv)
 
-    for i in range(1, num):
-        print("{}: {}".format(i, sys.argv[i]))
+    if count == 1:
+        print("{} arguments.".format(count - 1))
+    elif count == 2:
+        print("{} argument:".format(count - 1))
+        print("{}: {}".format(count - 1, sys.argv[count - 1]))
+    else:
+        print("{} arguments:".format(count - 1))
+
+        for i in range(1, count):
+            print("{}: {}".format(i, sys.argv[i]))
