e76efb1d1dd7e1d670145dc5a0cf4cb3d8823de1
JasperZeroes <jasperobed@gmail.com>
2022-06-03 03:18:17 +0000

    command line dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
-    #!/usr/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
     num = len(sys.argv)
     if num == 1:
         print("{} arguments.".format(num - 1))
@@ -11,31 +12,3 @@ if __name__ == "__main__":
 
     for i in range(1, num):
         print("{}: {}".format(i, sys.argv[i]))
-    
-    
-    '''parser = argparse.ArgumentParser()
-    parser.add_argument("number1", help = "first number")
-    parser.add_argument("number2", help = "second number")
-    parser.add_argument("operation", help = "operation")
-
-    args = parser.parse_args()
-
-    print(args.number1)
-    print(args.number2)
-    print(args.operation)
-    print(len(args))'''
-
-    '''if args.operation == "add":
-        result = int(args.number1) + int(args.number2)
-        print("Result:", result)
-    elif args.operation == "subtract":
-        result = int(args.number1) - int(args.number2)
-        print("Result:", result)
-    elif args.operation == "multiply":
-        result = int(args.number1) * int(args.number2)
-        print("Result:", result)
-    elif args.operation == "division":
-        result = int(args.number1) / int(args.number2)
-        print("Result:", int(result))
-    else:
-        print("Unsupported Operation")'''
