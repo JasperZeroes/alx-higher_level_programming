6068f0b2a93eec87a7fdd88e1092d1e9bfad909c
JasperZeroes <jasperobed@gmail.com>
2022-06-03 03:12:36 +0000

    command line dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,41 @@
-#!/usr/bin/python3
+    #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    count = len(sys.argv)
-
-    if count == 1:
-        print("{} arguments.".format(count - 1))
-    elif count == 2:
-        print("{} argument:".format(count - 1))
-        print("{}: {}".format(count - 1, sys.argv[count - 1]))
+    num = len(sys.argv)
+    if num == 1:
+        print("{} arguments.".format(num - 1))
+    elif num == 2:
+        print("{} argument:".format(num - 1))
     else:
-        print("{} arguments:".format(count - 1))
+        print("{} arguments:".format(num - 1))
 
-        for i in range(1, count):
-            print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, num):
+        print("{}: {}".format(i, sys.argv[i]))
+    
+    
+    '''parser = argparse.ArgumentParser()
+    parser.add_argument("number1", help = "first number")
+    parser.add_argument("number2", help = "second number")
+    parser.add_argument("operation", help = "operation")
+
+    args = parser.parse_args()
+
+    print(args.number1)
+    print(args.number2)
+    print(args.operation)
+    print(len(args))'''
+
+    '''if args.operation == "add":
+        result = int(args.number1) + int(args.number2)
+        print("Result:", result)
+    elif args.operation == "subtract":
+        result = int(args.number1) - int(args.number2)
+        print("Result:", result)
+    elif args.operation == "multiply":
+        result = int(args.number1) * int(args.number2)
+        print("Result:", result)
+    elif args.operation == "division":
+        result = int(args.number1) / int(args.number2)
+        print("Result:", int(result))
+    else:
+        print("Unsupported Operation")'''
