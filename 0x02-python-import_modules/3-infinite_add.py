1ec99e7b1a63d2038a45468325d26f2886ca9941
JasperZeroes <jasperobed@gmail.com>
2022-06-03 03:35:56 +0000

    added large numbers easily

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    count = len(sys.argv)
+    result = 0
+
+    if count == 1:
+        print(f"{result}")
+    else:
+        for i in range(1, count):
+            result += int(sys.argv[i])
+
+        print(f"{result}")
+'''parser = argparse.ArgumentParser()
+    parser.add_argument("number1", help = "first number")
+    parser.add_argument("number2", help = "second number")
+    parser.add_argument("operation", help = "operation")
+
+    args = parser.parse_args()
+
+    print(args.number1)
+    print(args.number2)
+    print(args.operation)
+    print(len(args))
+
+    if args.operation == "add":
+        result = int(args.number1) + int(args.number2)
+        print("Result:", result)
+    elif args.operation == "subtract":
+        result = int(args.number1) - int(args.number2)
+        print("Result:", result)
+    elif args.operation == "multiply":
+        result = int(args.number1) * int(args.number2)
+        print("Result:", result)
+    elif args.operation == "division":
+        result = int(args.number1) / int(args.number2)
+        print("Result:", int(result))
+    else:
+        print("Unsupported Operation")'''
