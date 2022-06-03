a8f0b80169df92ae0c217c958d0ef01af5b1d5b0
JasperZeroes <jasperobed@gmail.com>
2022-06-03 01:57:21 +0000

    command line dynamic scripts

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    count = len(sys.argv)
+    counter = 0
+
+    if count == 1:
+        print("0 arguments.")
+    elif count == 2:
+        print("{} argument".format(count - 1))
+        counter += 1
+        print(counter, ": ", sys.argv[counter])
+    else:
+        print(len(sys.argv) - 1, "arguments:")
+
+        for i in range(1, len(sys.argv)):
+            counter += 1
+            print(counter, ": ", sys.argv[i])
