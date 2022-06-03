31adfd7401e16ebd218bd55ec18f983cbcdfa031
JasperZeroes <jasperobed@gmail.com>
2022-06-03 04:11:32 +0000

    added large numbers easily

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-
+import sys
+def add(args):
     count = len(sys.argv)
     result = 0
 
-    '''if count == 1:
-        print("{}".format(result))
-    else:'''
-    for i in range(1, count):
-        result += int(sys.argv[i])
-    print("{}".format(result))
+    if count > 1:
+        for i in range(1, count):
+            result += int(sys.argv[i])
+        return result
+    else:
+        return result
+
+if (__name__ == '__main__'):
+    print(add(sys.argv[1:]))
