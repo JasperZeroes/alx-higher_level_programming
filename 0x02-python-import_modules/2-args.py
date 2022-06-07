9d54b1579ffeeeab900cd3c75497a50e9aef5fb4
JasperZeroes <Jasperobed@gmail.com>
2022-06-07 16:24:27 -0700

    made file 2 executable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,13 +11,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count - 1))
 
-        num = len(argv)
-        if num == 1:
-            print("{} arguments.".format(num - 1))
-        elif num == 2:
-            print("{} argument:".format(num - 1))
-        else:
-            print("{} arguments:".format(num - 1))
-
-        for i in range(1, num):
+        for i in range(1, count):
             print("{}: {}".format(i, argv[i]))
