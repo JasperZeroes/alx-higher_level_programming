d15e0e9f5a83eec1486964c01a55944bbffcef13
JasperZeroes <jasperobed@gmail.com>
2022-06-07 03:41:31 +0000

    added file for task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,7 @@ def multiple_returns(sentence):
     if not sentence:
         return len(sentence), None
     return len(sentence), sentence[0]
+
+
 '''Alternatively using list comprehension
     return (len(sentence), sentence[0] if len(sentence) > 0 else None)'''
