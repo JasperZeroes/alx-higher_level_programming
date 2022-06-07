f5ab4ef10027e8540c3704804521f44f656c98e7
JasperZeroes <jasperobed@gmail.com>
2022-06-07 16:38:06 +0000

    added file for task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    num = len(sys.argv)
+    if num == 1:
+        print("{} arguments.".format(num - 1))
+    elif num == 2:
+        print("{} argument:".format(num - 1))
+    else:
+        print("{} arguments:".format(num - 1))
+
+    for i in range(1, num):
+        print("{}: {}".format(i, sys.argv[i]))
+
+'''if __name__ == "__main__":
+    import sys
 
     count = len(sys.argv)
     if count == 1:
@@ -12,4 +25,4 @@ if __name__ == "__main__":
         print("{} arguments:".format(count - 1))
 
         for i in range(1, count):
-            print("{}: {}".format(i, sys.argv[i]))
+            print("{}: {}".format(i, sys.argv[i]))'''
