75982b9f43ded7b552ecc390cb1dc98872e3fb3d
JasperZeroes <Jasperobed@gmail.com>
2022-06-07 16:18:27 -0700

    made file 2 executable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     from sys import argv
 
-    num = len(argv)
-    if num == 1:
-        print("{} arguments.".format(num - 1))
-    elif num == 2:
-        print("{} argument:".format(num - 1))
-    else:
-        print("{} arguments:".format(num - 1))
-
-    for i in range(1, num):
-        print("{}: {}".format(i, argv[i]))
-
-'''if __name__ == "__main__":
-    import sys
-
-    count = len(sys.argv)
+    count = len(argv)
     if count == 1:
         print("{} arguments.".format(count - 1))
     elif count == 2:
         print("{} argument:".format(count - 1))
-        print("{}: {}".format(count - 1, sys.argv[count - 1]))
+        print("{}: {}".format(count - 1, argv[count - 1]))
     else:
         print("{} arguments:".format(count - 1))
 
-        for i in range(1, count):
-            print("{}: {}".format(i, sys.argv[i]))'''
+        num = len(argv)
+        if num == 1:
+            print("{} arguments.".format(num - 1))
+        elif num == 2:
+            print("{} argument:".format(num - 1))
+        else:
+            print("{} arguments:".format(num - 1))
+
+        for i in range(1, num):
+            print("{}: {}".format(i, argv[i]))
