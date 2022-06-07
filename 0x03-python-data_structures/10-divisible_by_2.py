8ff7d3b683aa3ae5017165485f178859725f5ff9
JasperZeroes <jasperobed@gmail.com>
2022-06-07 04:42:38 +0000

    added file for task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    return list(map(lambda x: True if x % 2 == 0 else False, my_list))
+
+
+''' Alternatively using lambda
     new_list = []
 
     for i in range(len(my_list)):
@@ -8,9 +12,4 @@ def divisible_by_2(my_list=[]):
         else:
             new_list.append("False")
     return new_list
-
-
-''' Alternatively using lambda
-#!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    return list(map(lambda x: True if x % 2 == 0 else False, my_list))'''
+'''
