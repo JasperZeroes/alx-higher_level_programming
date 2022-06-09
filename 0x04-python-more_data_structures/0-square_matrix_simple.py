71572d0c232d704f036a154f538ac642b66ac89d
JasperZeroes <jasperobed@gmail.com>
2022-06-09 02:51:12 +0000

    added README.md and file 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix = []):
+    new_matrix = []
+    for row in matrix:
+        empty = []
+        for i in range(len(row)):
+            empty.append(row[i] ** 2)
+        new_matrix.append(empty)
+    return new_matrix
+
+    '''def square_matrix_simple(matrix=[]):
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
+    return new_matrix'''
