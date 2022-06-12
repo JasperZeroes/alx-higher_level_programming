1d638e34ae6839def4480ff7d93d9c333dd0caf8
JasperZeroes <jasperobed@gmail.com>
2022-06-12 11:01:38 +0000

    added some code to file 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -7,3 +7,22 @@ def search_replace(my_list, search, replace):
         else:
             copy.append(my_list[i])
     return copy
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+search = 2
+replace = 89
+new_list = search_replace(my_list, search, replace)
+print(new_list)
+print(my_list)
+
+''' OR
+def search_replace(my_list, search, replace):
+    new_list = []
+    for num in my_list:
+        if num == search:
+            num = replace
+            new_list.append(num)
+        else:
+            new_list.append(num)
+    return new_list
+    '''
