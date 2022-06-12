ff3b06bbae070ecc383e0a73e1285deea5329ba0
JasperZeroes <jasperobed@gmail.com>
2022-06-12 11:36:28 +0000

    added code to file 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,3 +5,16 @@ def uniq_add(my_list=[]):
     for i in new:
         res += i
     return res
+'''
+def uniq_add(my_list=[]):
+    new_list = []
+    result = 0
+
+    for num in my_list:
+    if num not in new_list:
+        new_list.append(num)
+
+    for num in new_list:
+        result += num
+    return result
+'''
