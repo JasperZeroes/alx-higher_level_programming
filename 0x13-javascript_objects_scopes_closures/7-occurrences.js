12b396c1a0f5f20859b9f340f2cc9cd7e3331f90
JasperZeroes <jasperobed@gmail.com>
2024-04-11 15:58:48 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-exports.nbOccurences = function (list, searchElement) {
-  let count = 0;
-  for (const e of list) {
-    count = (e === searchElement) ? count + 1 : count;
-  }
-  return count;
-};
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-exports.esrever = function (list) {
-  const reverse = [];
-  list.forEach(e => reverse.unshift(e));
-  return reverse;
-};
